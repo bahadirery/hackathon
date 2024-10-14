d95366247b460e43a77e288f2e4b9e2de99ea6b2
baheryilmaz <bahadirery@gmail.com>
2024-10-14 16:39:49 +0200

    commit bahadir

--- got_ocr.py
+++ got_ocr.py
@@ -1,12 +1,12 @@
 from transformers import AutoModel, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
-model = model.eval().cpu()
+model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+model = model.eval().cuda()
 
 
 # input your test image
-image_file = '/Users/bahadir/Desktop/macbook_phd/hackathon/Bowers (1).jpg'
+image_file = 'Bowers.jpg'
 
 # plain texts OCR
 res = model.chat(tokenizer, image_file, ocr_type='ocr')
