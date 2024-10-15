f4b74da802176eb3efd85134ec4b0d13243e224f
baheryilmaz <bahadirery@gmail.com>
2024-10-15 11:35:27 +0200

    qwen

--- got_ocr.py
+++ got_ocr.py
@@ -1,14 +1,17 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
+
 from transformers import AutoModel, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+output_image_file = '/home/baheryilmaz/projects/hackathon/example_snippet.png'
+
+
+#Load the tokenizer and model for OCR
+tokenizer = AutoTokenizer.from_pretrained('stepfun-ai/GOT-OCR2_0', trust_remote_code=True)
+model = AutoModel.from_pretrained('stepfun-ai/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
 model = model.eval().cuda()
 
-
-# input your test image
-image_file = 'Bowers.jpg'
-
-# plain texts OCR
-res = model.chat(tokenizer, image_file, ocr_type='ocr')
+# Perform OCR on the converted PNG file
+res = model.chat(tokenizer, output_image_file, ocr_type='ocr')
 
 print(res)
